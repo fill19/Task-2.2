cfc2e95642ec51b94eedfcab04909de11e5de41f
Dmytro <65898747+fill19@users.noreply.github.com>
2020-11-12 20:35:33 +0200

    Task 2.2
    Task 2.2

---
+++ Task2/src/com/cod/java/Task2.java
@@ -0,0 +1,59 @@
+package com.cod.java;
+
+
+public class Task2 {
+    public static void main(String[] args) {
+        System.out.println(" All arithmetical operations: ");
+        System.out.println(" ");
+
+        short num1 = 34, num2 = 53;
+        System.out.println("");
+        System.out.println("Short results: ");
+        System.out.println("");
+        System.out.println("num1 + num2 = " + (num1 + num2));
+        System.out.println("num1 - num2 = " + (num1 - num2));
+        System.out.println("num1 / num2 = " + (num1 / num2));
+        System.out.println("num1 * num2 = " + (num1 * num2));
+
+
+        long num3 = -53255235L, num4 = 325252L;
+        System.out.println("");
+        System.out.println("Long results: ");
+        System.out.println("");
+        System.out.println("num3 + num4 = " + (num3 + num4));
+        System.out.println("num3 - num4 = " + (num3 - num4));
+        System.out.println("num3 / num4 = " + (num3 / num4));
+        System.out.println("num3 * num4 = " + (num3 * num4));
+
+        double num5 = 43.53, num6 = 425.567;
+        System.out.println("");
+        System.out.println("Double results: ");
+        System.out.println("");
+        System.out.println("num5 + num6 = " + (num5 + num6));
+        System.out.println("num5 - num6 = " + (num5 - num6));
+        System.out.println("num5 / num6 = " + (num5 / num6));
+        System.out.println("num5 * num6 = " + (num5 * num6));
+
+        float num7 = 953.5f, num8 = 5453.42f;
+        System.out.println("");
+        System.out.println("Float results: ");
+        System.out.println("");
+        System.out.println("num7 + num8 = " + (num7 + num8));
+        System.out.println("num7 - num8 = " + (num7 - num8));
+        System.out.println("num7 / num8 = " + (num7 / num8));
+        System.out.println("num7 * num8 = " + (num7 * num8));
+
+        byte num9 = 8, num10 = -5;
+        System.out.println("");
+        System.out.println("Byte results: ");
+        System.out.println("");
+        System.out.println("num9 + num10 = " + (num9 + num10));
+        System.out.println("num9 - num10 = " + (num9 - num10));
+        System.out.println("num9 / num10 = " + (num9 / num10));
+        System.out.println("num9 * num10 = " + (num9 * num10));
+
+
+        System.out.println("The task is done.");
+
+    }
+}
